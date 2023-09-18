b84e7b82f55f524a94de1c24cf6f5b4b5b018b4b
tamim-jr <internationalinf24@gmail.com>
2023-09-19 00:06:03 +0600

    remove some js code

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 "use client"
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from 'next-themes'
+// import { ThemeProvider } from 'next-themes'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -13,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" class="dark">
       <body className={inter.className}>
-      <ThemeProvider enableSystem={true} attribute='class'>
+      {/*<ThemeProvider enableSystem={true} attribute='class'>
+  </ThemeProvider>*/}
       {children}
-      </ThemeProvider>
       </body>
     </html>
   )
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,11 @@
 "use client";
 import React from 'react'
 import { BsFacebook, BsGithub, BsLinkedin } from "react-icons/bs";
-import { useState, useEffect } from 'react';
-import { BsFillSunFill, BsMoonFill } from "react-icons/bs";
-import { useTheme } from 'next-themes'
+// import { useState, useEffect } from 'react';
+// import { BsFillSunFill, BsMoonFill } from "react-icons/bs";
+// import { useTheme } from 'next-themes'
 const Footer = () => {
-  const { systemTheme, theme, setTheme } = useTheme();
+  {/*const { systemTheme, theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -14,11 +14,11 @@ const Footer = () => {
 
   if (!mounted) return null;
 
-  const currentTheme = theme === 'system' ? systemTheme : theme;
+const currentTheme = theme === 'system' ? systemTheme : theme;*/}
   
   return (
     <div className=' flex flex-col md:flex-row justify-between text-center p-2 md:px-16'>
-      <div className='fixed right-[20px] bottom-[20px] flex justify-center'>
+      {/*<div className='fixed right-[20px] bottom-[20px] flex justify-center'>
         {currentTheme === 'dark' ? (
           <button
             className='p-1 ' onClick={() => setTheme('light')}
@@ -28,7 +28,7 @@ const Footer = () => {
           className='p-1 ' onClick={() => setTheme('dark')}
         ><BsMoonFill />
         </button>)}
-      </div>
+        </div>*/}
       <div>
         <p className='text-sm py-2 px-8 md:px-0'>© 2023 Tamim Mahmud | Web Developer | All right reserved.</p>
       </div>
